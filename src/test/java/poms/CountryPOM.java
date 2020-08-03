51622da1c88297724e508ad991a40c8b7f44b35a
daulet2030 <daulet2030@gmail.com>
2020-08-03 22:38:59 +0600

    day 26 refactoring

--- src/test/java/poms/CountryPOM.java
+++ src/test/java/poms/CountryPOM.java
@@ -2,14 +2,6 @@ package poms;
 
 import org.openqa.selenium.By;
 
-public class CountryPOM extends BasePOM {
-
-    public By createButtonLocator = By.cssSelector("[data-icon=\"plus\"]");
-    public By editButtonLocator = By.cssSelector("ms-edit-button > button");
-    public By nameInputLocator = By.cssSelector("[formcontrolname=\"name\"] > input");
-    public By saveButtonLocator = By.tagName("ms-save-button");
-    public By searchButtonLocator = By.tagName("ms-search-button");
-    public By alertDialogLocator = By.cssSelector("div[role='alertdialog']");
-    public By nameSearchLocator = By.cssSelector("ms-text-field[placeholder='GENERAL.FIELD.NAME'] > input");
+public class CountryPOM extends TablePOM {
 
 }
---
+++ src/test/java/poms/TablePOM.java
@@ -0,0 +1,15 @@
+package poms;
+
+import org.openqa.selenium.By;
+
+public class TablePOM extends BasePOM {
+
+    public By createButtonLocator = By.cssSelector("[data-icon=\"plus\"]");
+    public By editButtonLocator = By.cssSelector("ms-edit-button > button");
+    public By nameInputLocator = By.cssSelector("[formcontrolname=\"name\"] > input");
+    public By saveButtonLocator = By.tagName("ms-save-button");
+    public By searchButtonLocator = By.tagName("ms-search-button");
+    public By alertDialogLocator = By.cssSelector("div[role='alertdialog']");
+    public By nameSearchLocator = By.cssSelector("ms-text-field[placeholder='GENERAL.FIELD.NAME'] > input");
+
+}
