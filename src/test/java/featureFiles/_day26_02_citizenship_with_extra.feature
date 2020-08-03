b7992eb3d3edf9a80cf74c23015e6ae5e2486b62
daulet2030 <daulet2030@gmail.com>
2020-08-03 22:30:14 +0600

    day 26 rest of scenarios of citizenship

--- src/test/java/featureFiles/_day26_02_citizenship_with_extra.feature
+++ src/test/java/featureFiles/_day26_02_citizenship_with_extra.feature
@@ -9,19 +9,19 @@ Feature: Full citizenship feature test
     When I create "Brazilia" citizenship
     Then citizenship is successfully created
 
-#  Scenario: Successful citizenship edit in basqar
-#    Given I navigate to basqar
-#    When I try to login using username and password
-#    Then I am logged in
-#    Given I navigate to citizenship screen
-#    And "Almania" citizenship doesn't exist
-#    When I edit  "Brazilia" to "Almania" citizenship
-#    Then citizenship is successfully edited
-#
-#  Scenario: Successful citizenship deletion in basqar
-#    Given I navigate to basqar
-#    When I try to login using username and password
-#    Then I am logged in
-#    Given I navigate to citizenship screen
-#    When I delete "Almania" citizenship
-#    Then citizenship is successfully deleted
+  Scenario: Successful citizenship edit in basqar
+    Given I navigate to basqar
+    When I try to login using username and password
+    Then I am logged in
+    Given I navigate to citizenship screen
+    And "Almania" citizenship doesn't exist
+    When I edit  "Brazilia" to "Almania" citizenship
+    Then citizenship is successfully edited
+
+  Scenario: Successful citizenship deletion in basqar
+    Given I navigate to basqar
+    When I try to login using username and password
+    Then I am logged in
+    Given I navigate to citizenship screen
+    When I delete "Almania" citizenship
+    Then citizenship is successfully deleted
--- src/test/java/steps/_03_CitizenshipSteps.java
+++ src/test/java/steps/_03_CitizenshipSteps.java
@@ -9,11 +9,11 @@ import poms.CitizenshipPOM;
 import poms.MenuPOM;
 
 public class _03_CitizenshipSteps {
-    private CitizenshipPOM citizenship;
+    private CitizenshipPOM citizenshipPage;
 
     @Given("^I navigate to citizenship screen$")
     public void i_navigate_to_citizenship_screen()  {
-        citizenship =new CitizenshipPOM();
+        citizenshipPage =new CitizenshipPOM();
         MenuPOM menu = new MenuPOM();
         menu.waitAndClick(menu.setupMenuLocator);
         menu.waitAndClick(menu.parametersMenuLocator);
@@ -23,23 +23,60 @@ public class _03_CitizenshipSteps {
 
     @And("^\"([^\"]*)\" citizenship doesn't exist$")
     public void citizenshipDoesnTExist(String citizenshipName) {
-        citizenship.waitAndSendKeys(citizenship.nameSearchLocator, citizenshipName);
-        citizenship.waitAndClick(citizenship.searchButtonLocator);
-        citizenship.waitForProgressBar();
+        citizenshipPage.waitAndSendKeys(citizenshipPage.nameSearchLocator, citizenshipName);
+        citizenshipPage.waitAndClick(citizenshipPage.searchButtonLocator);
+        citizenshipPage.waitForProgressBar();
 
-        citizenship.deleteAllElementsFromTable();
+        citizenshipPage.deleteAllElementsFromTable();
     }
 
     @Then("^citizenship is successfully created$")
     public void citizenship_is_successfully_created()  {
-        String actual = citizenship.waitForNewAndGetText(citizenship.alertDialogLocator);
+        String actual = citizenshipPage.waitForNewAndGetText(citizenshipPage.alertDialogLocator);
         Assert.assertEquals(actual, "Citizenship successfully created");
     }
 
     @When("^I create \"([^\"]*)\" citizenship$")
     public void i_create_citizenship(String citizenshipName) {
-        citizenship.waitAndClick(citizenship.createButtonLocator);
-        citizenship.waitAndSendKeys(citizenship.nameInputLocator, citizenshipName);
-        citizenship.waitAndClick(citizenship.saveButtonLocator);
+        citizenshipPage.waitAndClick(citizenshipPage.createButtonLocator);
+        citizenshipPage.waitAndSendKeys(citizenshipPage.nameInputLocator, citizenshipName);
+        citizenshipPage.waitAndClick(citizenshipPage.saveButtonLocator);
+    }
+
+    @When("^I edit  \"([^\"]*)\" to \"([^\"]*)\" citizenship$")
+    public void iEditToCitizenship(String oldCitizenship, String newCitizenship) {
+        citizenshipPage.waitAndSendKeys(citizenshipPage.nameSearchLocator, oldCitizenship);
+        citizenshipPage.waitAndClick(citizenshipPage.searchButtonLocator);
+
+        citizenshipPage.waitForProgressBar();
+
+        citizenshipPage.waitAndClick(citizenshipPage.editButtonLocator);
+        citizenshipPage.waitAndSendKeys(citizenshipPage.nameInputLocator, newCitizenship);
+        citizenshipPage.waitAndClick(citizenshipPage.saveButtonLocator);
+    }
+
+    @Then("^citizenship is successfully edited$")
+    public void citizenship_is_successfully_edited() {
+        String actual = citizenshipPage.waitForNewAndGetText(citizenshipPage.alertDialogLocator);
+        Assert.assertEquals(actual, "Citizenship successfully updated");
+    }
+
+    @When("^I delete \"([^\"]*)\" citizenship$")
+    public void iDeleteCitizenship(String citizenshipName) throws Throwable {
+        citizenshipPage.waitAndSendKeys(citizenshipPage.nameSearchLocator, citizenshipName);
+        citizenshipPage.waitAndClick(citizenshipPage.searchButtonLocator);
+
+        citizenshipPage.waitForProgressBar();
+
+        citizenshipPage.waitAndClick(citizenshipPage.deleteButtonLocator);
+        citizenshipPage.waitAndClick(citizenshipPage.dialogSubmitButtonLocator);
+    }
+
+
+    @Then("^citizenship is successfully deleted$")
+    public void citizenship_is_successfully_deleted() {
+        String actual = citizenshipPage.waitForNewAndGetText(citizenshipPage.alertDialogLocator);
+        Assert.assertEquals(actual, "Citizenship successfully deleted");
+        // TODO: the list doesn't contain the citizenship that we deleted
     }
 }
