9e655ee7e5b92903f68dfa91597281e849093da6
daulet2030 <daulet2030@gmail.com>
2020-08-13 20:41:14 +0600

    day 32 example of Scenario injectable

--- src/test/java/exampleRunners/RegressionExamplesRunner.java
+++ src/test/java/exampleRunners/RegressionExamplesRunner.java
@@ -4,6 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.BeforeMethod;
 
 @CucumberOptions(
         features = {
@@ -14,5 +15,4 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
         tags = {"@RegressionTest"}
 )
 public class RegressionExamplesRunner extends AbstractTestNGCucumberTests {
-
 }
--- src/test/java/steps/FirstSteps.java
+++ src/test/java/steps/FirstSteps.java
@@ -1,5 +1,6 @@
 package steps;
 
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
@@ -11,11 +12,11 @@ public class FirstSteps {
     }
 
     @Before // Before each scenario
-    public void before(){
-        System.out.println("Before scenario!");
+    public void before(Scenario scenario){
+        System.out.println("Before scenario: " + scenario.getName());
     }
     @After // After each scenario
-    public void after(){
-        System.out.println("After scenario!");
+    public void after(Scenario scenario){
+        System.out.println("After scenario: " + scenario.getName());
     }
 }
