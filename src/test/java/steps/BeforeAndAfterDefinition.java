ee44033991509b94141b23da9c7cd46105f918a0
daulet2030 <daulet2030@gmail.com>
2020-08-14 21:34:49 +0600

    day 33 extent screen shot embedding

--- src/test/java/steps/BeforeAndAfterDefinition.java
+++ src/test/java/steps/BeforeAndAfterDefinition.java
@@ -1,5 +1,6 @@
 package steps;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -7,18 +8,25 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utils.BaseDriver;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.io.FileUtils.copyFile;
+
 public class BeforeAndAfterDefinition {
     @Before // Before each scenario
     public void before(Scenario scenario){
         System.out.println("Before scenario: " + scenario.getName());
     }
     @After // After each scenario
-    public void after(Scenario scenario){
+    public void after(Scenario scenario) throws IOException {
         System.out.println("After scenario: " + scenario.getName());
         if (scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) BaseDriver.getDriver())
-                    .getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot, "image/png"); //stick it in the report
+            File sourcePath = ((TakesScreenshot) BaseDriver.getDriver()).getScreenshotAs(OutputType.FILE);
+            File destinationPath = new File(System.getProperty("user.dir") + "/target/extent_report/screenshots/" + scenario.getName() + ".png");
+            copyFile(sourcePath, destinationPath);
+
+            Reporter.addScreenCaptureFromPath("./screenshots/" + scenario.getName() + ".png");
         }
     }
 }
