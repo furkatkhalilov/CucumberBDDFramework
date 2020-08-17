38e4e8e8406832401ae2c58f542dbc77a5ba1404
daulet2030 <daulet2030@gmail.com>
2020-08-17 20:27:28 +0600

    reading excel, whole file

--- src/main/java/ReadingExcel.java
+++ src/main/java/ReadingExcel.java
@@ -6,14 +6,18 @@ import java.io.IOException;
 public class ReadingExcel {
     public static void main(String[] args) throws IOException {
         Workbook workbook = WorkbookFactory.create(new File("src/main/resources/data.xlsx"));
-        Sheet sheet = workbook.getSheet("list");
 
-        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
-            Row row = sheet.getRow(i);
-            for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
-                Cell cell = row.getCell(j);
-                System.out.print(cell.getStringCellValue() + " ");
+        for (int n = 0; n < workbook.getNumberOfSheets(); n++) {
+            Sheet sheet = workbook.getSheetAt(n);
+            for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+                Row row = sheet.getRow(i);
+                for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+                    Cell cell = row.getCell(j);
+                    System.out.print(cell.getStringCellValue() + " ");
+                }
+                System.out.println();
             }
+            System.out.println();
             System.out.println();
         }
     }
