6a17c6e94e6105f1d2c6cf71c4c0ef4e331b74bd
daulet2030 <daulet2030@gmail.com>
2020-08-17 22:15:49 +0600

    cucumber, reading from excel, map scenario

--- src/test/java/steps/ExcelSteps.java
+++ src/test/java/steps/ExcelSteps.java
@@ -6,7 +6,9 @@ import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelSteps {
 
@@ -21,8 +23,26 @@ public class ExcelSteps {
         for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
             Row row = sheet.getRow(i); // get reference to
             Cell cell = row.getCell(0);
-            list.add(cell.getStringCellValue());
+            list.add(cell.toString());
         }
         System.out.println(list);
+    }
+
+
+    @Given("^I printout to console from excel using map only$")
+    public void iPrintoutToConsoleFromExcelUsingMapOnly() throws IOException {
+        Map<String,String> map = new HashMap<>();
+
+        File file = new File("src/main/resources/data.xlsx");
+        Workbook workbook = WorkbookFactory.create(file);
+
+        Sheet sheet = workbook.getSheet("map");
+        for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+            Row row = sheet.getRow(i); // get reference to
+            Cell cell0 = row.getCell(0);
+            Cell cell1 = row.getCell(1);
+            map.put(cell0.toString(),cell1.toString()); // key, value
+        }
+        System.out.println(map);
     }
 }
