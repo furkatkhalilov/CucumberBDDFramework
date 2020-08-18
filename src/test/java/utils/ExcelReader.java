1b878b1030d6c894f07880696803497ed24669ff
daulet2030 <daulet2030@gmail.com>
2020-08-18 21:47:15 +0600

    task4: ExcelReader reusable class

---
+++ src/test/java/utils/ExcelReader.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelReader {
+    Workbook workbook;
+    public ExcelReader(String filePath){
+
+    }
+
+    public List<String> getList(String sheetName) {
+        return null;
+    }
+
+    public Map<String, String> getMap(String sheetName) {
+        return null;
+    }
+
+    public List<List<String>> getListOfLists(String sheetName) {
+        return null;
+    }
+
+    public List<Map<String, String>> getListOfMaps(String sheetName) {
+        return null;
+    }
+}
